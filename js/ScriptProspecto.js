6ce89aa45fa89fc260c25d8ec113b0a32ec027ce
AlbertoZegers <101838814+AlbertoZegers@users.noreply.github.com>
2023-12-11 18:19:13 -0300

    Update ScriptProspecto.js

--- js/ScriptProspecto.js
+++ js/ScriptProspecto.js
@@ -5,7 +5,7 @@ document.getElementById("user-form").addEventListener("submit", function(event) 
     const rut = document.getElementById("rut").value;
     const estado = document.getElementById("estado").value;
   
-    console.log("Prospecto cerrado:");
+    console.log("Prospecto creado:");
     console.log("Nombre del cliente: ", username);
     console.log("Rut: ", rut);
     console.log("Estado: ", estado); 
